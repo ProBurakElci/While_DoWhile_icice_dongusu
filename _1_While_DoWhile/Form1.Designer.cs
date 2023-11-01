65bfe59344e744b632e1b134205ade646999c07e
Burak Elçi <burakelci12@gmail.com>
2023-11-01 17:08:32 +0300

    Proje dosyası ekle.

---
+++ _1_While_DoWhile/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _1_While_DoWhile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWhileDongusu = new System.Windows.Forms.Button();
+            this.btnDoWhileDongusu = new System.Windows.Forms.Button();
+            this.btnIcIceDongu = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnWhileDongusu
+            // 
+            this.btnWhileDongusu.Location = new System.Drawing.Point(16, 32);
+            this.btnWhileDongusu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnWhileDongusu.Name = "btnWhileDongusu";
+            this.btnWhileDongusu.Size = new System.Drawing.Size(144, 26);
+            this.btnWhileDongusu.TabIndex = 0;
+            this.btnWhileDongusu.Text = "WHILE DONGUSU";
+            this.btnWhileDongusu.UseVisualStyleBackColor = true;
+            this.btnWhileDongusu.Click += new System.EventHandler(this.btnWhileDongusu_Click);
+            // 
+            // btnDoWhileDongusu
+            // 
+            this.btnDoWhileDongusu.Location = new System.Drawing.Point(16, 81);
+            this.btnDoWhileDongusu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDoWhileDongusu.Name = "btnDoWhileDongusu";
+            this.btnDoWhileDongusu.Size = new System.Drawing.Size(144, 33);
+            this.btnDoWhileDongusu.TabIndex = 1;
+            this.btnDoWhileDongusu.Text = "DO WHİLE DÖNGÜSÜ";
+            this.btnDoWhileDongusu.UseVisualStyleBackColor = true;
+            this.btnDoWhileDongusu.Click += new System.EventHandler(this.btnDoWhileDongusu_Click);
+            // 
+            // btnIcIceDongu
+            // 
+            this.btnIcIceDongu.Location = new System.Drawing.Point(16, 133);
+            this.btnIcIceDongu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIcIceDongu.Name = "btnIcIceDongu";
+            this.btnIcIceDongu.Size = new System.Drawing.Size(144, 28);
+            this.btnIcIceDongu.TabIndex = 2;
+            this.btnIcIceDongu.Text = "İÇ İÇE DÖNGÜ";
+            this.btnIcIceDongu.UseVisualStyleBackColor = true;
+            this.btnIcIceDongu.Click += new System.EventHandler(this.btnIcIceDongu_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 190);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 116);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnIcIceDongu);
+            this.Controls.Add(this.btnDoWhileDongusu);
+            this.Controls.Add(this.btnWhileDongusu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWhileDongusu;
+        private System.Windows.Forms.Button btnDoWhileDongusu;
+        private System.Windows.Forms.Button btnIcIceDongu;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ _1_While_DoWhile/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_While_DoWhile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWhileDongusu_Click(object sender, EventArgs e)
+        {
+            int i = 10;
+            while (i < 20)
+            {
+                // NOT : şart ifadesindeki değişkenin değeri scope içinde değiştirilmelidir. yoksa sürekli true sonuç üretileceği içimn, sonsuz döngü olur.
+                listBox1.Items.Add("merhaba");
+                i++;
+            }
+        }
+
+        private void btnDoWhileDongusu_Click(object sender, EventArgs e)
+        {
+            //do
+            //{
+            //} while (true);
+            //// NOT : do while döngüsünde, döngünün dönmeye devam edip etmeyeceği, en sonda kontrol edilir.
+            //en sondaki while ifadesinin şart sonucu true ise döngü döner, false ise devam etmez.
+            int i = 0;
+            do
+            {
+                listBox1.Items.Add("a");
+            } while (i < 0);
+
+            while (i < 0)
+            {
+                listBox1.Items.Add("b");
+            }
+            // NOT : do while döngüsünde, ilk defa çalışırken giriş için kontrol yapılmadığından dolayı,
+            //ekrana 1 kere a yazacaktır. while da ise girişte kontrol yapılacak, sonuç false olacağı için föndü çalışmayacak ve ekrana b yazılmayaxcaktır. Yani do while döngüsü EN AZ 1 KERE ÇALIŞIR.
+        }
+        int sayac_a, sayac_b;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIcIceDongu_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                listBox1.Items.Add("a");
+                sayac_a++;
+                for (int j = 2; j < 15; j += 3)
+                {
+                    listBox1.Items.Add("b");
+                    sayac_b++;
+                }
+                listBox1.Items.Add("c");
+            }
+            MessageBox.Show("a : " + sayac_a + " b : " + sayac_b);
+            // NOT : İçteki döngünün çalışması bitmeden, dıştaki döngü devam etmez.
+
+        }
+    }
+}
